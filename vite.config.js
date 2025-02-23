3d63de8b82bb241a5fdf73c483fd5c1057f69cee
PRO-BOY1 <157917472+PRO-BOY1@users.noreply.github.com>
2025-02-23 12:02:11 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": "https://law-back-end.up.railway.app/",
-    },
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   },
 });
